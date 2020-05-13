84318f8222663865da406311e15034f64ad0b0a3
Tiger Wang <ziwei.tiger@outlook.com>
2020-05-13 22:06:25 +0100

    Address comments

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -8,7 +8,10 @@ ARGS="-header-filter $REGEX -quiet -export-fixes $FIXES_FILE "$@" $REGEX"
 
 mkdir -p tidy-build
 cd tidy-build
-cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DPRECOMPILE_HEADERS=OFF ..
+
+# Disable precompiled headers since they aren't generated during linting which causes an error
+# Disable unity builds since clang-tidy needs the full list of compiled files to check each one
+cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DPRECOMPILE_HEADERS=OFF -DUNITY_BUILDS=OFF ..
 
 if run-clang-tidy $ARGS; then
 	echo "clang-tidy: No violations found"
